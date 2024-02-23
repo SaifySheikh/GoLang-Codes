bd5505515e4142c5d8a4dc3c992dbc24a45cf83e
SaifySheikh <sheikhmm_4@rknec.edu>
2024-02-23 08:48:44 +0530

    sumRecursion added

--- 23feb/sumRecursion.go
+++ 23feb/sumRecursion.go
@@ -13,8 +13,13 @@ func main() {
 	var num int
 	fmt.Println("Enter a number : ")
 	fmt.Scan(&num)
+	if(num<0){
+		fmt.Println("Please enter a valid number")
+	}else{
+		var result=calSum(num)
+		fmt.Println(result)
+	}
 
-	var result=calSum(num)
-	fmt.Println(result)
+	
 
 }	
